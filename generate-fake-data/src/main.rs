204f2b661ed1e44494546831042a2d97753f35f7
Lars Mühmel <larsmuehmel@web.de>
2024-05-30 22:33:35 +0200

    make generate-fake-data reproducible
    this allows us to purge the huge migration
    from the git repo

--- generate-fake-data/src/main.rs
+++ generate-fake-data/src/main.rs
@@ -1,5 +1,6 @@
 use anyhow::Result;
 use fake::{locales::EN, Fake};
+use rand::{Rng, SeedableRng};
 use std::{
     collections::HashSet,
     fs::File,
@@ -12,21 +13,23 @@ fn main() -> Result<()> {
         .and_then(|a| a.parse().ok())
         .unwrap_or(9_000);
 
+    let rng = rand::rngs::SmallRng::from_seed(*b"A cool seed that's 32 bytes long");
+
     match std::env::args().nth(2) {
         Some(path) => {
             let file = File::create(path)?;
             let out = BufWriter::new(file);
-            write(out, num)?;
+            write(out, num, rng)?;
         }
         None => {
             let out = std::io::stdout().lock();
-            write(out, num)?;
+            write(out, num, rng)?;
         }
     }
     Ok(())
 }
 
-fn write<B: Write>(mut out: B, num: i64) -> Result<()> {
+fn write<B: Write, R: Rng>(mut out: B, num: i64, mut rng: R) -> Result<()> {
     write!(
         out,
         "INSERT INTO Contacts (first, last, phone, email) VALUES "
@@ -36,14 +39,14 @@ fn write<B: Write>(mut out: B, num: i64) -> Result<()> {
 
     'outer: for n in 0..num {
         eprintln!("generating {n}th entry");
-        let mut first: &'static str = fake::faker::name::raw::FirstName(EN).fake();
-        let mut last: &'static str = fake::faker::name::raw::LastName(EN).fake();
-        let phone: String = fake::faker::phone_number::raw::PhoneNumber(EN).fake();
+        let mut first: &'static str = fake::faker::name::raw::FirstName(EN).fake_with_rng(&mut rng);
+        let mut last: &'static str = fake::faker::name::raw::LastName(EN).fake_with_rng(&mut rng);
+        let phone: String = fake::faker::phone_number::raw::PhoneNumber(EN).fake_with_rng(&mut rng);
         let mut email: String = format!("{first}.{last}@example.com");
         let mut tries = 1;
         while used_emails.contains(&email) {
-            first = fake::faker::name::raw::FirstName(EN).fake();
-            last = fake::faker::name::raw::LastName(EN).fake();
+            first = fake::faker::name::raw::FirstName(EN).fake_with_rng(&mut rng);
+            last = fake::faker::name::raw::LastName(EN).fake_with_rng(&mut rng);
             email = format!("{first}.{last}@example.com");
             tries += 1;
             if tries % 1000 == 0 {
