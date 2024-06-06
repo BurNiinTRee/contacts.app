4b450a51e48ae6bfd5789a3a04c221e7b3639010
Lars Mühmel <larsmuehmel@web.de>
2024-06-06 19:22:14 +0200

    No more --impure 🎉, commented out fenix

--- flake.nix
+++ flake.nix
@@ -2,16 +2,21 @@
   inputs = {
     bntr.url = "github:BurNiinTRee/nix-sources?dir=modules";
     devenv.url = "github:cachix/devenv";
-    fenix.url = "github:nix-community/fenix";
+    devenv-root = {
+      url = "file+file:///dev/null";
+      flake = false;
+    };
+    # fenix.url = "github:nix-community/fenix";
     flake-parts.url = "github:hercules-ci/flake-parts";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
   outputs = inputs @ {
     flake-parts,
-    fenix,
+    # fenix,
     bntr,
     devenv,
+    devenv-root,
     nixpkgs,
     ...
   }:
@@ -21,13 +26,18 @@
       imports = [bntr.flakeModules.nixpkgs devenv.flakeModule];
 
       perSystem = {
-        nixpkgs.overlays = [fenix.overlays.default];
+        # nixpkgs.overlays = [fenix.overlays.default];
         devenv.shells.default = {
           lib,
           pkgs,
           config,
           ...
         }: {
+          devenv.root = let
+            devenvRootFileContent = builtins.readFile devenv-root.outPath;
+          in
+            pkgs.lib.mkIf (devenvRootFileContent != "") devenvRootFileContent;
+
           # https://github.com/cachix/devenv/issues/528
           containers = lib.mkForce {};
           languages.rust = {
